8fe0cb21de9dfd7e5347057b4a51f387b8ffb15e
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-05-22 15:45:01 +0200

    entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,25 @@ if [ "$executable" = "null" ];then
 	executable=$(jq .main package.json)
 	if [ "$executable" != "null" ];then
 		echo $executable executable_js
+		return $executable
 	else
 		echo no main
 		if [ -e index.js ];then
 			executable=index.js
 			echo index.js found $executable
+			return $executable
 		else
 			echo no index.js found
 			if [ -e app.js ];then
 				executable=app.js
 				echo app.js found $executable
+				return $executable
 			else
 				echo no app.js found
 				if [ -e server.js ];then
 					executable=server.js
 					echo server.js found $executable
+					return $executable
 				else
 					echo no entrypoint found
 				fi
@@ -30,4 +34,5 @@ if [ "$executable" = "null" ];then
 	fi
 else
 	echo start script $executable
+	return $executable
 fi
