9c2baafee34dbc2b944e8607be3bc13ac5228b34
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-05-24 20:47:58 +0200

    name change for s3 bucket artefact

--- infrastructure/deploy_artefact_storage.tf
+++ infrastructure/deploy_artefact_storage.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "s3_bucket_deploy_artefact" {
-  bucket = "${var.stack}-${var.git_project}"
+  bucket = "${var.stack}-${var.git_project}-docker-artefact"
   force_destroy = true
   acl = "private"
-  region = "${var.region}"
+  region = "${var.region}"cd
 
 }
 resource "aws_s3_bucket_object" "deploy_artefact" {
