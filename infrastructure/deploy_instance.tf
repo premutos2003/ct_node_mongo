98a3c8cf012bb3ac5e3d5d7fae6b890bfc152f19
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-08-31 16:47:55 +0200

    edit

--- infrastructure/deploy_instance.tf
+++ infrastructure/deploy_instance.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "instance" {
       "aws --version",
       "sudo yum update -y",
       "sudo yum install docker -y",
-      "curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo usermod -a -G docker ec2-user",
       "sudo chkconfig docker on",
